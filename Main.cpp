702078720b7b97c96f758e51e29ecc26e61fd429
Sinosha <dtf@mail.ru>
2022-04-18 20:45:57 +0500

    Third commit.
    -added init function.
    -added get coordinates function.
    -created AI_move function.

--- Main.cpp
+++ Main.cpp
@@ -1,10 +1,38 @@
 #include <iostream>
+#include<cctype>
 
 using namespace std;
 
+char board[3][3];
 enum {EMPTY = ' ',CROSS = 'X', ZERO = 'O' };
+char player_char;
+char computer_char;
 
+void init()
+{
+	cout << "Choose prefered symbol to play with (X or O) \n";
+	do {
+		cin >> player_char;
+		player_char = toupper(player_char);
+	} while (player_char != 'X' && player_char != 'O');
 
+	computer_char = (player_char == 'X' ? 'O' : 'X');
+}
+
+void get_coords(int& row, int& col) 
+{
+	do {
+		cout << "Write coordinates (from 1 to 3) \n";
+		cin >> col >> row;
+		col--;
+		row--;
+	} while (col < 0 || col > 2 || row < 0 || row > 2 || board[row][col] != EMPTY);
+}
+
+void AI_move()
+{
+
+}
 
 void print(char board[3][3])
 {
